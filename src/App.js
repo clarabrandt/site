0e5d5a0957f69a7e436be314a0b0c8a05cb95f46
clarabrandt <mariaclarabrandt@gmail.com>
2018-12-10 22:46:47 +0100

    Create react app

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import Courses from './components/Courses.js'
+import Work from './components/Work.js'
+import Studies from './components/Studies.js'
+import Skills from './components/Skills.js'
+import Objectives from './components/Objectives.js'
+import Title from './components/Title.js'
+import Subtitle from './components/Subtitle.js'
+import Picture from './components/Picture.js'
+import About from './components/About.js'
+import Languages from './components/Languages.js'
+import Interests from './components/Interests.js'
+
+// import MainContainer from './components/MainContainer';
+
+class App extends Component {
+  render() {
+    return (
+            <div className="App">
+              <div className='grid'>
+                <Title />
+                <Subtitle />
+                <Picture />
+                <About />
+                <Languages />
+                <Interests />
+                <Objectives />
+                <Studies />
+                <Courses />
+                <Skills />
+                <Work />
+              </div>
+            </div>
+    );
+  }
+}
+
+export default App;
