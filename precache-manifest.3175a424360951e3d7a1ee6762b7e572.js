71e5c65489e25dd64bf884261caecff80d3186f8
clarabrandt <mariaclarabrandt@gmail.com>
2019-01-04 18:36:34 +0100

    Updates

---
+++ precache-manifest.3175a424360951e3d7a1ee6762b7e572.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "038d54b256d224697e3a3b43d4c883d4",
+    "url": "/site/static/media/me.038d54b2.JPG"
+  },
+  {
+    "revision": "5571d5242120e713b47d",
+    "url": "/site/static/js/runtime~main.5571d524.js"
+  },
+  {
+    "revision": "0b30115c831bc9424b07",
+    "url": "/site/static/js/main.0b30115c.chunk.js"
+  },
+  {
+    "revision": "06b310f500357581fd60",
+    "url": "/site/static/js/1.06b310f5.chunk.js"
+  },
+  {
+    "revision": "0b30115c831bc9424b07",
+    "url": "/site/static/css/main.3ec54cf8.chunk.css"
+  },
+  {
+    "revision": "43cfe42b3da601860616a8f5628cf04c",
+    "url": "/site/index.html"
+  }
+];
