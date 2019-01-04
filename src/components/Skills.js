db77529601a442b5ba56890d06147b79478c4e11
clarabrandt <mariaclarabrandt@gmail.com>
2019-01-04 14:44:15 +0100

    aligning the Studies topic

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,7 +10,7 @@ class Skills extends Component {
         <div className="skills--myskills">
           <div className='skills-topic'>HTML5</div>
           <div className='skills-topic'>CSS: <span className='skills--specifics '>flex-box, grid</span></div>
-          <div className='skills-topic'>'JavaScript:' <span className='skills--specifics '>React, Redux</span></div>
+          <div className='skills-topic'>JavaScript: <span className='skills--specifics '>React, Redux</span></div>
           <div className='skills-topic'>Node JS</div>
           <div className='skills-topic'>Express</div>
           <div className='skills-topic'>LCA Softwares: <span className='skills--specifics '>SimaPro, Gabi</span></div>
--- src/components/Studies.js
+++ src/components/Studies.js
@@ -8,26 +8,26 @@ class Studies extends Component {
         <div className="studies--title">STUDIES</div>
         <div className="studies--line"></div>
         <div className="studies--content">
-          <div className='studies--item'>
-            <div className="studies--date">2018 </div>
-            <div className='studies--topic'>Codaisseur</div>
-            <div className='studies--school'>Full stack web development course</div>
-          </div>
-          <div className='studies--item'>
-            <div className="studies--date">2012 - 2017 </div>
-            <div className='studies--topic'>PhD Environmental and Energy Planning</div>
-            <div className='studies--school'>Federal University of Rio de Janeiro</div>
-          </div>
-          <div className='studies--item'>
-            <div className="studies--date">2010 - 2012</div>
-            <div className='studies--topic'>MSc Environmental and Energy Planning</div>
-            <div className='studies--school'>Federal University of Rio de Janeiro</div>
-          </div>
-          <div className='studies--item'>
-            <div className="studies--date">2005 - 2009</div>
-            <div className='studies--topic'>Bachelor in Biology </div>
-            <div className='studies--school'>Federal University of Rio de Janeiro</div>
-          </div>
+          <div className="studies--date">2018 </div>
+            <div className='studies--item'>
+              <div className='studies--topic'>Codaisseur</div>
+              <div className='studies--school'>Full stack web development course</div>
+            </div>
+          <div className="studies--date">2012 - 2017 </div>
+            <div className='studies--item'>
+              <div className='studies--topic'>PhD Environmental and Energy Planning</div>
+              <div className='studies--school'>Federal University of Rio de Janeiro</div>
+            </div>
+          <div className="studies--date">2010 - 2012</div>
+            <div className='studies--item'>
+              <div className='studies--topic'>MSc Environmental and Energy Planning</div>
+              <div className='studies--school'>Federal University of Rio de Janeiro</div>
+            </div>
+          <div className="studies--date">2005 - 2009</div>
+            <div className='studies--item'>
+              <div className='studies--topic'>Bachelor in Biology </div>
+              <div className='studies--school'>Federal University of Rio de Janeiro</div>
+            </div>
         </div>
       </div>
     )
